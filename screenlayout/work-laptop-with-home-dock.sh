be5ae73b6e0144d9b4d88491f7f53afb1f16f4bc
Andy Galkin <galk@google.com>
2021-04-17 14:37:27 -0700

    update for kitty and i3

--- screenlayout/work-laptop-with-home-dock.sh
+++ screenlayout/work-laptop-with-home-dock.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --refresh 59.97
+xrandr --output eDP-1 --mode 3840x2160 --refresh 59.97 --scale 0.5x0.5
 xrandr --output DP-2-2 --primary  --mode 3840x2160 --right-of eDP-1 
 xrandr --output DP-2-3 --mode 1920x1080 --right-of DP-2-2 --refresh 59.94
