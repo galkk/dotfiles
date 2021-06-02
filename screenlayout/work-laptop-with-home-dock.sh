77cb83d0ba704c0985676f70a7f5221811a1a1fa
Andy Galkin <galk@google.com>
2021-06-02 12:47:03 -0700

    changed position and orientation of displays

--- screenlayout/work-laptop-with-home-dock.sh
+++ screenlayout/work-laptop-with-home-dock.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 3840x2160 --refresh 59.97 --scale 0.5x0.5
-xrandr --output DP-2-2 --primary  --mode 3840x2160 --right-of eDP-1 
-xrandr --output DP-2-3 --mode 1920x1080 --right-of DP-2-2 --refresh 59.94
+xrandr --output DP-2-3 --primary --mode 2560x1440  --refresh 59.98
+xrandr --output eDP-1 --mode 1920x1080 --refresh 59.97 --right-of DP-2-3
+xrandr --output DP-2-2 --mode 2560x1440 --left-of DP-2-3
+
