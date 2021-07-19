b900c5b269f30fa9cbd8d01c3aea51c807a90d07
Andy Galkin <galk@google.com>
2021-07-19 11:56:43 -0700

    changed position and orientation of displays

--- screenlayout/work-3-monitors.sh
+++ screenlayout/work-3-monitors.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-2-1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-2-2 --mode 2560x1440 --pos 1080x0 --rotate normal --output DP-2-3 --mode 2560x1440 --pos 3640x0 --rotate normal
+xrandr --output eDP-1 --off \
+    --output DP-1 --off \
+    --output HDMI-1 --off \
+    --output DP-2 --off \
+    --output DP-2-1 --mode 1920x1080 --pos 0x0 --rotate right \
+    --output DP-2-2 --mode 2560x1440 --right-of DP-2-1 --rotate normal --primary \
+    --output DP-2-3 --mode 2560x1440 --right-of DP-2-2 --rotate normal
