e5a9246b42944854be54bd5e914e6ee71ddc9d50
Andy Galkin <galk@google.com>
2021-07-20 09:12:01 -0700

    changed xrandr parameters

--- screenlayout/work-3-monitors.sh
+++ screenlayout/work-3-monitors.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-xrandr --output eDP-1 --off \
-    --output DP-1 --off \
-    --output HDMI-1 --off \
-    --output DP-2 --off \
-    --output DP-2-1 --mode 1920x1080 --pos 0x0 --rotate right \
-    --output DP-2-2 --mode 2560x1440 --right-of DP-2-1 --rotate normal --primary \
-    --output DP-2-3 --mode 2560x1440 --right-of DP-2-2 --rotate normal
+xrandr  --output eDP-1  --off \
+        --output DP-1   --off \
+        --output HDMI-1 --off \
+        --output DP-2   --off
+
+xrandr  --output DP-2-1 --mode 1920x1080 --pos 0x0 --rotate right
+xrandr  --output DP-2-2 --mode 2560x1440 --right-of DP-2-1 --rotate normal --primary
+xrandr  --output DP-2-3 --mode 2560x1440 --right-of DP-2-2 --rotate normal
