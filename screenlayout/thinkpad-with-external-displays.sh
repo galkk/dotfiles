48922eecf8b4192980dc6a935c6edaf779413d46
Andy Galkin <andy.galkin@gmail.com>
2021-07-31 13:55:28 -0700

    minor tweaks:

--- screenlayout/thinkpad-with-external-displays.sh
+++ screenlayout/thinkpad-with-external-displays.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-xrandr	--output DP-1.2 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
-		--output DP-1.3 --mode 2560x1440 --pos 3840x0 --rotate normal --rate 144.0 \
-		--output DP-0 --off --output DP-1 --off --output HDMI-0 --off
+xrandr --output DP-1.1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1.3 --mode 2560x1440 --pos 5120x0 --rotate right --output DP-1.2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output eDP-1-1 --off
