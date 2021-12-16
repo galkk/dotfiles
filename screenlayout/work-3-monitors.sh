1e5cf58da02951760122077d4c04f145eb9ea9f6
Andy Galkin <galk@google.com>
2021-12-15 17:44:09 -0800

    changed xrandr parameters, added config for only laptop

--- screenlayout/work-3-monitors.sh
+++ screenlayout/work-3-monitors.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-xrandr  --output eDP-1  --off \
-        --output DP-1   --off \
-        --output HDMI-1 --off \
-        --output DP-2   --off
-
-xrandr  --output DP-2-1 --mode 2560x1440 --pos 0x0 --rotate normal
-xrandr  --output DP-2-2 --mode 2560x1440 --right-of DP-2-1 --rotate normal --primary
-xrandr  --output DP-2-3 --mode 1920x1080 --right-of DP-2-2 --rotate right
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-2-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2-2 --mode 2560x1440 --pos 1920x0 --rotate normal --primary --output DP-2-3 --mode 2560x1440 --pos 4480x0 --rotate left
