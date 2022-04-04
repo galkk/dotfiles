2624fb151a0427d142c4b24de782f86367ff3a24
Andy Galkin <andy.galkin@gmail.com>
2022-04-04 13:19:09 -0700

    some changes in config

--- screenlayout/work-2-monitors.sh
+++ screenlayout/work-2-monitors.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-2-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2-2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-2-3 --off
+xrandr --output eDP-1 --off \
+  --output DP-1 --off \
+  --output HDMI-1 --off \
+  --output DP-2 --off \
+  --output DP-2-1 --mode 2560x1440 --pos 0x0 --rotate normal \
+  --output DP-2-2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --rate 99.95 \
+  --output DP-2-3 --off
