3c7dfae66dae011bc9afbcc1bd7b8fd7bac7dc1c
Andy Galkin <andy.galkin@gmail.com>
2023-01-25 20:43:09 -0800

    configuration for home ubuntu 3 monitors desktop

---
+++ screenlayout/home-3-monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --primary --mode 3840x2160 --pos 2560x0 --rotate normal --output DP-0 --mode 2560x1440 --pos 6400x0 --rotate left --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 0x0 --rotate normal -r 120.00 --output DP-5 --off
