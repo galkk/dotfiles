d388a99f40bf5bfb6dc49ba06ba8a23756bd4f89
Andy Galkin <andy.galkin@gmail.com>
2023-02-19 14:12:36 -0800

    migrated from makefile to init scripts

---
+++ 1-install.sh
@@ -0,0 +1,9 @@
+apt update
+
+apt install -y zsh curl git \
+    peco openssh-server wget \
+    fzf htop mc mosh vim cmake python3-dev golang \
+    clang build-essential sc jq lldb strace rr \
+    highlight imagemagick poppler-utils ffmpeg \
+    command-not-found neovim ripgrep
+
---
+++ 3-install-gui.sh
@@ -0,0 +1,13 @@
+apt install -y fonts-firacode fonts-dejavu fonts-hack-ttf fonts-powerline
+wget https://rubjo.github.io/victor-mono/VictorMonoAll.zip
+unzip -j VictorMonoAll.zip TTF/* -d ~/.fonts
+rm VictorMonoAll.zip
+
+# TODO(galk): add azeret mono
+
+
+apt install -y i3 rofi flameshot remmina xinit brightnessctl kitty peek \
+	copyq
+
+# to run brightnessctl without sudo
+usermod -aG video $USER
