f9f1e4f0e196a4fadf93dee6b3e8034e13029a94
Andy Galkin <andy.galkin@gmail.com>
2023-02-19 15:51:40 -0800

    minor

--- 4-install-gui.sh
+++ 4-install-gui.sh
@@ -1,10 +1,12 @@
 apt install -y fonts-firacode fonts-dejavu fonts-hack-ttf fonts-powerline
+
+# Install Victor Mono
+
 wget https://rubjo.github.io/victor-mono/VictorMonoAll.zip
 unzip -j VictorMonoAll.zip TTF/* -d ~/.fonts
 rm VictorMonoAll.zip
 
 # TODO(galk): add azeret mono
-
 
 apt install -y i3 rofi flameshot remmina xinit brightnessctl kitty peek \
 	copyq
