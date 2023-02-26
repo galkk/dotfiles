9223eb8e5eb43e4055314c0118c3c183b8bd272f
Andy Galkin <andy.galkin@gmail.com>
2023-02-26 00:23:20 -0800

    added bat installation in full install

--- 3-install-full.sh
+++ 3-install-full.sh
@@ -1,6 +1,10 @@
 apt install -y zsh curl git \
-    peco openssh-server wget \
+    openssh-server wget \
     fzf htop mc mosh vim cmake python3-dev golang \
-    clang build-essential sc jq lldb strace rr \
+    clang build-essential jq lldb strace rr \
     highlight imagemagick ffmpeg \
-    command-not-found neovim ripgrep
+    command-not-found neovim ripgrep bat
+
+# like, but not really necessary
+# command line spreadsheet calculator
+# apt install sc
