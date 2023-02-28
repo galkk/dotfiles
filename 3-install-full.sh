630f7dc02e15c22bb0d6e9dfcef0e836d2e6e726
Andy Galkin <andy.galkin@gmail.com>
2023-02-28 13:15:07 -0800

    Added podman and config

--- 3-install-full.sh
+++ 3-install-full.sh
@@ -3,7 +3,11 @@ apt install -y zsh curl git \
     fzf htop mc mosh vim cmake python3-dev golang \
     clang build-essential jq lldb strace rr \
     highlight imagemagick ffmpeg \
-    command-not-found neovim ripgrep bat
+    command-not-found neovim ripgrep bat podman
+
+echo "
+[registries.search]
+registries = ['docker.io']" | tee -a /etc/containers/registries.conf
 
 # like, but not really necessary
 # command line spreadsheet calculator
