7e1791fc0ea89c2f75ca5bdc80ec47269f47d20a
Andy Galkin <andy.galkin@gmail.com>
2023-03-13 11:33:27 -0700

    added ubuntu-mate-desktop and chrome remote desktop

--- 4-install-gui.sh
+++ 4-install-gui.sh
@@ -9,7 +9,7 @@ rm VictorMonoAll.zip
 # TODO(galk): add https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
 
 apt install -y i3 rofi flameshot remmina xinit brightnessctl kitty peek \
-	copyq flatpak
+	copyq flatpak ubuntu-mate-desktop
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub md.obsidian.Obsidian
@@ -18,6 +18,11 @@ flatpak install -y flathub com.jetbrains.IntelliJ-IDEA-Community
 wget -O vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
 apt install -y ./vscode.deb
 rm vscode.deb
+
+# easy way out for remote access, no need to do xrdp/vnc all the stuff.
+# TODO(galk): consider doing some "proper" xrdp configuration later 
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+apt install ./chrome-remote-desktop_current_amd64.deb
 
 # TODO(galk): add repository for i3 latest and install i3 from there, to get things like gaps
 
