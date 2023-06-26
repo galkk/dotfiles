c5dd8f3f50d8c0f11b94831448d0d28b87814e42
Andy Galkin <andy.galkin@gmail.com>
2023-06-26 06:58:40 -0700

    moved font configuration to separate file

---
+++ setup/4-gui-fonts.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Fonts
+apt-get -qq install --no-install-recommends fnt unzip \
+    fonts-firacode fonts-dejavu fonts-hack-ttf
+
+fnt update
+fnt install azeretmono anonymouspro notosansmono robotomono cousine ibmplexmono
+
+# Victor Mono
+wget https://rubjo.github.io/victor-mono/VictorMonoAll.zip
+unzip -j VictorMonoAll.zip "TTF/*" -d ~/.fonts
+rm VictorMonoAll.zip
+
+# Nerd fonts symbols only, for kitty
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip
+unzip -j NerdFontsSymbolsOnly.zip "*.ttf" -d ~/.fonts
+rm NerdFontsSymbolsOnly.zip
+
+fc-cache -f -v
