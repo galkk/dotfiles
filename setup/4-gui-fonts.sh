c181d8ca0b7c775bfb1a22e89aded8e009d29793
Andy Galkin <andy.galkin@gmail.com>
2023-07-12 17:49:59 +0000

    Fixed font setup. Apparently fnt sucks a lot Added iosevka

--- setup/4-gui-fonts.sh
+++ setup/4-gui-fonts.sh
@@ -1,20 +1,35 @@
 #!/usr/bin/env bash
 
 # Fonts
-apt-get -qq install --no-install-recommends fnt unzip \
-    fonts-firacode fonts-dejavu fonts-hack-ttf
+apt-get -qq install --no-install-recommends fnt unzip subversion
 
 fnt update
-fnt install azeretmono anonymouspro notosansmono robotomono cousine ibmplexmono
 
-# Victor Mono
+# Change to `fnt install` after https://github.com/alexmyczko/fnt/issues/30 is fixed
+fonts=( anonymouspro \
+        cousine \
+        firacode \
+        ibmplexmono \
+)
+
+for font in ${fonts[@]}; do
+    fnt install $font
+done
+
+svn export --force https://github.com/googlefonts/RobotoMono/trunk/fonts/ttf ~/.fonts/
+svn export --force https://github.com/displaay/Azeret/trunk/fonts/ttf ~/.fonts/
+svn export --force https://github.com/ryanoasis/nerd-fonts/trunk/master/patched-fonts/NerdFontsSymbolsOnly ~/.fonts/
+svn export --force https://github.com/notofonts/notofonts.github.io/trunk/fonts/NotoSansMono/unhinted/ttf ~/.fonts/
+
+chdir $(mktemp -d)
 wget https://rubjo.github.io/victor-mono/VictorMonoAll.zip
-unzip -j VictorMonoAll.zip "TTF/*" -d ~/.fonts
-rm VictorMonoAll.zip
+wget https://github.com/dejavu-fonts/dejavu-fonts/releases/download/version_2_37/dejavu-fonts-ttf-2.37.zip
+wget https://github.com/be5invis/Iosevka/releases/download/v25.0.1/ttf-iosevka-25.0.1.zip
+wget https://github.com/be5invis/Iosevka/releases/download/v25.0.1/ttf-iosevka-fixed-25.0.1.zip
+wget https://github.com/be5invis/Iosevka/releases/download/v25.0.1/ttf-iosevka-term-25.0.1.zip
 
-# Nerd fonts symbols only, for kitty
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip
-unzip -j NerdFontsSymbolsOnly.zip "*.ttf" -d ~/.fonts
-rm NerdFontsSymbolsOnly.zip
+unzip -j '*.zip'
+mv *.ttf ~/.fonts
 
 fc-cache -f -v
+
