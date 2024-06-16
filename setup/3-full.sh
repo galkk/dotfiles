1d7394654f1d462c2a5b153a54e1c0fb6827c50a
Andy Galkin <andy.galkin@gmail.com>
2024-06-16 07:36:02 -0700

    removed exa (obsolete)

--- setup/3-full.sh
+++ setup/3-full.sh
@@ -4,8 +4,8 @@ apt-get -qq install --no-install-recommends openssh-server htop mc mosh \
     build-essential cmake python3-dev golang \
     jq strace highlight imagemagick ffmpeg \
     neovim ripgrep podman podman-toolbox \
-    openjdk-17-jdk exa tmux wget \
-    clang lldb gcc-13 g++-13 gdb rr
+    openjdk-17-jdk tmux wget \
+    clang lldb gcc g++ gdb rr
 
 # Let podman to get images from docker hub.
 echo "
